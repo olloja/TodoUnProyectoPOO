edc1ac68932cd3f7b1f4819e8819b719b5b237c0
RO\Rosario-Omar <omarlj@upeu.edu.pe>
2018-01-04 16:06:42 -0500

    16:06pm Ollojapress

--- src/GUI/NewClass1.java
+++ src/GUI/NewClass1.java
@@ -11,4 +11,9 @@ package GUI;
 public class NewClass1 {
     /*Mensaje de Netbeans 8.0.2*/
     /*Mendsaje desde NetBeand 7.1.2*/
+    //System.out.println("Hola");
+    public static void main(String[] args)
+    {
+        //System.out.println("Hola");
+    }
 }
